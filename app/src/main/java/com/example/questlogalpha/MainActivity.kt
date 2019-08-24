1c72717b6ab02ad6ff1d3c62b171b62abd4fa947
Kat Sayrs <zymphe2@gmail.com>
2019-08-24 11:40:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/questlogalpha/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.questlogalpha
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.questlogalpha.databinding.ActivityMainBinding
+import com.example.questlogalpha.ui.main.SectionsPagerAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // left/right scrolling. This activity probably won't actually need anything else.
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        binding.viewPager.adapter = sectionsPagerAdapter
+        binding.tabs.setupWithViewPager(binding.viewPager)
+
+
+        // other things in the main activity
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
