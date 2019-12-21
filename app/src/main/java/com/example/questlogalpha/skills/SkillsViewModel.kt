f2ef95497e20be70b9dabe3e7c2dd16ee38a644a
Kat Sayrs <sayrs2@gmail.com>
2019-12-21 10:50:37 -0800

    Adding add debug skill to test button, to test Personnage view

--- app/src/main/java/com/example/questlogalpha/skills/SkillsViewModel.kt
+++ app/src/main/java/com/example/questlogalpha/skills/SkillsViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.questlogalpha.data.Skill
+import com.example.questlogalpha.data.SkillType
 import com.example.questlogalpha.personnage.SkillsDao
 import kotlinx.coroutines.*
 
@@ -16,19 +17,48 @@ class SkillsViewModel (val database: SkillsDao) : ViewModel() {
     /**
      * TODO add navigation
      */
- // private val _navigateToViewEditQuest = MutableLiveData<String?>()
- // val navigateToViewEditQuest: LiveData<String?> get() = _navigateToViewEditQuest
+    private val _navigateToViewEditSkill = MutableLiveData<String?>()
+    val navigateToViewEditSkill: LiveData<String?> get() = _navigateToViewEditSkill
 
  // /**
- //  * Call this immediately after navigating to [ViewEditQuestFragment]
+ //  * Call this immediately after navigating to [ViewEditSkillFragment]
  //  */
     fun doneNavigating() {
-  //     _navigateToViewEditQuest.value = null
+       _navigateToViewEditSkill.value = null
     }
 
     init {
         Log.d(TAG," initiated")
     }
+
+    // public functions
+    // ---------------------------------------------------------------- //
+
+    fun onAddDebugSkill()
+    {
+        // todo launch to new screen -- ViewEditSkill
+        viewModelScope.launch {
+            insert(Skill("NewSkill", 0, SkillType.NONE))
+        }
+    }
+
+    fun onAddSkill() {
+        viewModelScope.launch {
+            Log.d("$TAG onAddSkill", "Logging")
+            _navigateToViewEditSkill.value = ""
+        }
+    }
+
+    fun onEditSkill(skill: Skill) {
+        viewModelScope.launch {
+            Log.d("$TAG onEditSkill", "Logging")
+            _navigateToViewEditSkill.value = skill.id
+        }
+    }
+
+
+    // private functions
+    // ---------------------------------------------------------------- //
 
     private suspend fun getAllSkills(): List<Skill>? {
         return withContext(Dispatchers.IO){
